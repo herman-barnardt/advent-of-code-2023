7140512bcd3efd510be05cf291b24d03d2e11d08
Herman Barnardt <herman.barnardt@momentum.co.za>
2023-12-18 06:30:22 +0200

    Solve day 17

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	_ "advent-of-code-2023/day13"
 	_ "advent-of-code-2023/day14"
 	_ "advent-of-code-2023/day15"
+	_ "advent-of-code-2023/day17"
 	_ "advent-of-code-2023/day2"
 	_ "advent-of-code-2023/day3"
 	_ "advent-of-code-2023/day4"
